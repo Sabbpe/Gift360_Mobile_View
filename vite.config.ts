7626816ecb42eb171d071792699380fc00fc68a5
Sabbpe <shubhang.b@sabbpe.com>
2026-07-22 19:32:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-
 export default defineConfig({
   appType: "spa",
   plugins: [react()],
@@ -21,6 +20,6 @@ export default defineConfig({
   preview: {
     host: "0.0.0.0",
     port: 6688,
-    allowedHosts: ["uat.gift360.io"],
+    allowedHosts: ["gift360.io", "www.gift360.io", "uat.gift360.io"],
   },
 });
